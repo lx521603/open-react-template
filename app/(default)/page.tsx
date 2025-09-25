57200d6a8a199e3d588d98322035e01b4ad5a4d7
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-09-25 22:17:46 +0800

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,12 +3,8 @@ export const metadata = {
   description: "Page description",
 };
 
-import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
-import Workflows from "@/components/workflows";
-import Features from "@/components/features";
-import Testimonials from "@/components/testimonials";
-import Cta from "@/components/cta";
+
 
 export default function Home() {
   return (
