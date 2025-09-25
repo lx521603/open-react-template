65dfc65ace08d7542ab36bff41614227500089d6
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-09-25 22:20:55 +0800

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,8 +3,12 @@ export const metadata = {
   description: "Page description",
 };
 
+import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
-
+import Workflows from "@/components/workflows";
+import Features from "@/components/features";
+import Testimonials from "@/components/testimonials";
+import Cta from "@/components/cta";
 
 export default function Home() {
   return (
